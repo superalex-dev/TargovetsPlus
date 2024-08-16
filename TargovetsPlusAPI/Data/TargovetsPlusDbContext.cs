4bc469d7eeda8a88b17b1c101fae16c6099abe4a
Alex Boev <naidobrixxjr@gmail.com>
2024-08-16 23:49:51 +0300

    ./.

--- TargovetsPlusAPI/Data/TargovetsPlusDbContext.cs
+++ TargovetsPlusAPI/Data/TargovetsPlusDbContext.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using TargovetsPlusAPI.Models;
 
 namespace TargovetsPlusAPI.Data
 {
@@ -8,29 +7,6 @@ namespace TargovetsPlusAPI.Data
         public TargovetsPlusDbContext(DbContextOptions<TargovetsPlusDbContext> options)
             : base(options)
         {
-        }
-
-        public DbSet<Customer> Customers { get; set; }
-        public DbSet<Product> Products { get; set; }
-        public DbSet<Sale> Sales { get; set; }
-        public DbSet<Inventory> Inventory { get; set; }
-
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            modelBuilder.Entity<Sale>()
-                .HasOne(s => s.Customer)
-                .WithMany()
-                .HasForeignKey(s => s.CustomerId);
-
-            modelBuilder.Entity<Sale>()
-                .HasOne(s => s.Product)
-                .WithMany(p => p.Sales)
-                .HasForeignKey(s => s.ProductId);
-
-            modelBuilder.Entity<Inventory>()
-                .HasOne(i => i.Product)
-                .WithMany()
-                .HasForeignKey(i => i.ProductId);
         }
     }
 }
